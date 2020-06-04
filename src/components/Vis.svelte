743cb730ef1c4aa9ea0d7c7eecaa188c513b31ed
Rodrigo Lima <rodrigo.sad.lima@gmail.com>
2020-06-04 13:49:06 -0300

    headline and svg

---
+++ src/components/Vis.svelte
@@ -0,0 +1,31 @@
+<script>
+  let width, height;
+  let margin = { top: 10, left: 10, right: 10, bottom: 10 };
+
+  $: innerWidth = width - margin.left - margin.right;
+  $: innerHeight = height - margin.top - margin.bottom;
+</script>
+
+<style>
+  div {
+    width: 90vw;
+    height: 80vh;
+    margin: auto;
+  }
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<div bind:clientWidth={width} bind:clientHeight={height}>
+
+  <svg>
+    <rect {width} {height} />
+    <g transform="translate({margin.left},{margin.top})">
+      <rect width={innerWidth} height={innerHeight} fill="white" />
+    </g>
+  </svg>
+
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,31 @@
+<script>
+  import Vis from "../components/Vis.svelte";
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 2em 0.5em 2em;
+    text-align: center;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  h2 {
+    font-size: 2em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 2em 0.5em 2em;
+    color: dimgray;
+    text-align: center;
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Music Industry</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>A Battle of Media!</h1>
+<h2>A history of music media formats and how they impacted industry</h2>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<Vis />
